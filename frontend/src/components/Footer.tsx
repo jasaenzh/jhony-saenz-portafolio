b81130e2ff849a97de2f47fe68c0e1f65a1eb85c
Jhony A Saenz <jash0310@gmail.com>
2023-12-18 20:22:19 -0500

    Agregando footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,9 +1,34 @@
+import IconLinkedin from "../assets/UilLinkedin.svg"
+import IconGithub from '../assets/AkarIconsGithubFill.svg'
+
 const Footer = () => {
   return (
-    <div className="bg-dark text-light text-center barra-footer">
-      <h5>&copy; Creado por Jhony Alberto Saenz Hurtado</h5>
-      <p>2023 - {new Date().getFullYear()}</p>
-    </div>
+    <footer className="bg-[#e7e9ec] text-[#e7e9ec] flex w-full z-30 shadow-lg">
+
+      <div className='flex flex-1 items-center justify-center'>
+        <div className='w-full md:w-4/12 px-4 mx-auto text-center'>
+          <div className='text-sm text-[#00AA4D] font-semibold py-1'>
+            Copyright © <span>2023 Jhony Saenz</span>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="flex gap-3 w-2/12 items-center justify-center pr-3 m-2">
+
+        {/* Linkedin */}
+        <a href="https://www.linkedin.com/in/jhony-saenz-hurtado" target='_blank' rel='noopener noreferrer'>
+          <img src={IconLinkedin} alt="Linkedin" className='w-10 h-10' />
+        </a>
+
+        {/* Github */}
+        <a href="https://github.com/jasaenzh" target='_blank' rel='noopener noreferrer'>
+          <img src={IconGithub} alt="Linkedin" className='w-10 h-10' />
+        </a>
+
+      </div>
+
+    </footer>
   );
 };
 
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -72,14 +72,15 @@ function Navbar() {
   }, [isAuthenticated])
 
   return (
-    <nav className="fixed w-full z30 top-0 bg-black shadow-md left-0 py-2 text-slate-50">
-      <div className="bg-slate-400 flex md:flex justify-between items-center py-4 sm:px-8">
+    <nav className="fixed w-full z30 top-0 bg-[#4D4D50] shadow-md left-0 py-3 text-[#e7e9ec]">
+      {/* Icono de Menu Hamburgesa */}
+      <div onClick={toggleMenu} className='text-2xl absolute right-6 top-1 cursor-pointer md:hidden text-[#00AA4D] border border-solid border-[#00AA4D] rounded-md pt-2 pl-2 pr-2 pb-1'>
+        <IonIcon icon={open ? `${close}` : `${menu}`} />
+      </div>
 
-        <div onClick={toggleMenu} className='text-4xl absolute right-7 top-8 cursor-pointer md:hidden text-[#7B5640]'>
-          <IonIcon icon={open ? `${close}` : `${menu}`} />
-        </div>
-
-        <ul className={`md:flex md:items-center md:pb-0 pb-10 absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-5 transition-all duration-500 ease-in ${open ? 'top-20 opacity-100' : 'top-[-490px]'}`} onClick={closeMenu} >
+      {/* Lista de enlaces */}
+      <div className="md:py-1 py-4 sm:px-8">
+        <ul className={`md:flex gap-16 justify-end md:items-center md:pb-0 pb-2 absolute md:static md:z-auto left-0 w-full md:pl-0 pl-5 transition-all duration-0 ease-in ${open ? 'top-16 opacity-100' : 'top-[-490px]'}`} onClick={closeMenu} >
           {linksToShow.map((link, index) => {
             return (
               <li key={index}>
